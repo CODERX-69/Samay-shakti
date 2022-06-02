02e4f7568551902441ec2340320d37ee0d0bf872
CODERX-69 <66686641+CODERX-69@users.noreply.github.com>
2022-06-02 15:02:59 +0530

    changes

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 10,
-    autoWidth:true,
-    nav: true,
+    autoWidth: true,
+    nav: false,
+    dots:false,
     autoplay: true,
     autoplayTimeout: 2000,
     smartSpeed: 1000,
@@ -22,38 +23,73 @@ $('.owl-carousel').owlCarousel({
 
 var $homeSlider = $(".home-slider");
 
-$(window).resize(function() {
-  showHomeSlider();
+$(window).resize(function () {
+    showHomeSlider();
 });
 
 function showHomeSlider() {
-  if ($homeSlider.data("owlCarousel") !== "undefined") {
-    if (window.matchMedia('(max-width: 600px)').matches) {
-        destroyHomeSlider();
-    } else {
-        initialHomeSlider();
+    if ($homeSlider.data("owlCarousel") !== "undefined") {
+        if (window.matchMedia('(max-width: 600px)').matches) {
+            destroyHomeSlider();
+        } else {
+            initialHomeSlider();
+        }
     }
-  }
 }
 showHomeSlider();
 
 function initialHomeSlider() {
-  $homeSlider.addClass("owl-carousel").owlCarousel({
-    items: 1,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 2000,
-    smartSpeed: 1000
-  });
+    $homeSlider.addClass("owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        smartSpeed: 1000
+    });
 }
 
 function destroyHomeSlider() {
-  $homeSlider.trigger("destroy.owl.carousel").removeClass("owl-carousel");
+    $homeSlider.trigger("destroy.owl.carousel").removeClass("owl-carousel");
 }
 
 
 
-  function menuToggle() {
-      const toggleMenu = document.querySelector('.pro-menu');
-      toggleMenu.classList.toggle('active')
-  }
+function menuToggle() {
+    const toggleMenu = document.querySelector('.pro-menu');
+    toggleMenu.classList.toggle('active')
+}
+
+window.addEventListener('mouseup', function(event){
+    var box = this.document.getElementById('drpdwn');
+    if(event.target != box && event.target.parentNode != box ){
+        box.style.display = 'none' ;
+
+    }
+});
+
+const $menu = $('#clickable');
+
+$(document).mouseup(e => {
+    if (!$menu.is(e.target) // if the target of the click isn't the container...
+        && $menu.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        $menu.removeClass('is-active');
+    }
+});
+
+$('.toggle').on('click', () => {
+    $menu.toggleClass('is-active');
+});
+
+
+
+
+const searchInput = document.querySelector("[data-search]")
+
+searchInput.addEventListener("input", e => {
+    const value = e.target.value 
+    console.log(value)
+})
+
+
+
